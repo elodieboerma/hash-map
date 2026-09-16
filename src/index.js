8e6b249d612ed610b415e6cfe7d3f7adf0f052c1
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-09-15 23:31:59 -0500

    make hash() function

--- src/index.js
+++ src/index.js
@@ -5,4 +5,14 @@ class HashMap {
     }
 }
 
+function hash(key,capacity) {
+    let hashCode = 0;
+    
+    let primeNumber  = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        hashCode = hashCode % capacity;
+    }
 
+    return hashCode;
+}
