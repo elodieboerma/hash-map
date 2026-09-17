6b64102cea06f8220bc9f4c104d5ffe0a69bc57e
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-09-16 23:47:28 -0500

    move functions to methods inside HashMap class

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ class HashMap {
         this.capacity = 16;
         this.loadFactor = 0.75;
     }
-}
 
-function hash(key,capacity) {
-    let hashCode = 0;
+    hash(key) {
+        let hashCode = 0;
     
-    let primeNumber  = 31;
-    for (let i = 0; i < key.length; i++) {
-        hashCode = primeNumber * hashCode + key.charCodeAt(i);
-        hashCode = hashCode % capacity;
+        let primeNumber  = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this.capacity;
+        }
+
+        return hashCode;
     }
 
-    return hashCode;
+    set(key, value) {
+        
+    }
 }
