16a73fb1289dfc57016b9af91d35d56f76a95fa4
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-09-17 00:01:28 -0500

    write set(key,value) method -- requires copying linked list and new node code into this project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      var current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+
+
 class HashMap {
     constructor() {
         this.capacity = 16;
@@ -17,6 +44,14 @@ class HashMap {
     }
 
     set(key, value) {
-        
+        let index = this.hash(key);
+        let bucket = this.at[index];
+        if (this.capacity * this.loadFactor >= this.size) {
+            this.capacity *= 2;
+        }
+        if (bucket == undefined) {
+            bucket = new LinkedList();
+        }
+        bucket.append(new Node(value));
     }
 }
